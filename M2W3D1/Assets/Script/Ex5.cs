6d439cebc9b061f884c4331a4b024ea956604257
ThinkDifferent003 <rolandbibal3@gmail.com>
2025-10-30 16:22:42 +0100

    ex5

---
+++ M2W3D1/Assets/Script/Ex5.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex5 : MonoBehaviour
+{
+    [SerializeField] int[] arraybase;
+    [SerializeField] int dimension;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] primoarray = CambiaDimensioneArray(arraybase , dimension);
+       
+
+    
+
+    }
+    int []CambiaDimensioneArray (int[] array, int dim)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (dim < array.Length)
+            {
+                int[] newarray = new int[dim];
+                newarray[i] = array[dim - 1];
+                Debug.Log("Array diminuito da : " + array.Length + ", a" + newarray.Length);
+                return newarray;
+            }
+            else if (dim >  array.Length)
+            {
+                int[] secondarray = new int[dim];
+                secondarray[i] = array[i];
+                Debug.Log("Array aumentato da : " + array.Length + ", a " + secondarray.Length );
+                return secondarray;
+            }
+            else if (dim == array.Length)
+            {
+                Debug.Log("Array uguale");
+                return array;
+            }
+        }
+        return array;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
